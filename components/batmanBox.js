cdbee67b6337c224418b6e9f23272c8df72bebd1
pawgog <p.gogola@interia.pl>
2020-04-17 19:49:32 +0200

    Add title, restyle boxes.

--- components/batmanBox.js
+++ components/batmanBox.js
@@ -1,16 +1,21 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function BatmanBox({ movie }) {
-    const batmanID = movie.show.id;
-    
-    return (
-        <div className='batman-block__content'>
-            <img src={ movie.show.image.medium } alt={ movie.show.name }></img>
-            <Link href={{ pathname: '/detail', query: { id: batmanID }}}>
-                <a>{ movie.show.name }</a>
-            </Link>
-            <p>{ movie.show.summary }</p>
-            <a className='batman-block__btn' href={ movie.show.url }><span>More...</span></a>
-        </div>
-    )
-};
+  const batmanID = movie.show.id;
+
+  return (
+    <div className="batman-block__content">
+      <img src={movie.show.image.medium} alt={movie.show.name}></img>
+      <Link href={{ pathname: '/detail', query: { id: batmanID } }}>
+        <a>{movie.show.name}</a>
+      </Link>
+      <div
+        className="batman-block__description"
+        dangerouslySetInnerHTML={{ __html: movie.show.summary }}
+      />
+      <a className="batman-block__btn" href={movie.show.url}>
+        <span>More...</span>
+      </a>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,35 @@ import Head from 'next/head';
 import 'isomorphic-unfetch';
 
 import BatmanBox from '../components/batmanBox';
-import '../scss/styles.scss'
- 
+import '../scss/styles.scss';
+
 export default class Movies extends Component {
   static async getInitialProps() {
-    const response = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const response = await fetch(
+      'https://api.tvmaze.com/search/shows?q=batman'
+    );
     const json = await response.json();
- 
-    return { movies: json }
+
+    return { movies: json };
   }
- 
+
   render() {
     return (
       <div>
         <Head>
-            <title>Next routing</title>
-            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <title>Next routing</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
         </Head>
-        <section className='batman-block'>
+        <h1>Batman Movies</h1>
+        <section className="batman-block">
           {this.props.movies.map((movie, index) => (
-            <BatmanBox
-              key={index}
-              movie={movie}
-            />
+            <BatmanBox key={index} movie={movie} />
           ))}
         </section>
       </div>
-    )
+    );
   }
 }
